b85706d210514bea0be29b1330934a8537d0554b
Filomena Tejeda <ftejeda@alumnos.uai.cl>
2025-04-03 12:31:23 -0300

    main

--- main1.c
+++ main1.c
@@ -3,18 +3,20 @@
 #include "structs.h"
 #include "lectura/lectura_csv.h" // Importamos la función leer_csv
 #include "funciones/pizza_mas_vendida.h"
+#include "funciones/pizza_menos_vendida.h"
+
 
 #define MAX_VENTAS 500
 
 int main(int argc, char *argv[]) {
     VentaPizza ventas[MAX_VENTAS];
-    int total_ventas = leer_csv("venta.csv", ventas, MAX_VENTAS);
+    int total_ventas = leer_csv(argv[1], ventas, MAX_VENTAS);
     
     // Imprimimos las primeras 10 ventas
     //for (int i = 0; i < 10 && i < total_ventas; i++) {
-        //printf("Venta %d - Pizza: %s, Cantidad: %.2f, Total: %.2f, Ingrediente: %s \n", 
-              // i + 1, ventas[i].pizza_name, ventas[i].quantity, ventas[i].unit_price, ventas[i].pizza_ingredients);
-   // }
+        //printf("Venta %d - Pizza: %s, Cantidad: %.2f, Total: %.2f, Ingrediente: %s \n, Fecha: %s", 
+              // i + 1, ventas[i].pizza_name, ventas[i].quantity, ventas[i].unit_price, ventas[i].pizza_ingredients, ventas[i].order_date);
+    //}
 
     if (total_ventas == 0) {
         printf("No se cargaron ventas.\n");
@@ -29,19 +31,16 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    if (strcmp(argv[1], "pms") == 0) {
-        pizza_mas_vendida(ventas, total_ventas);
-    } 
-    else if (strcmp(argv[1], "top3") == 0) {
-        // top3_pizzas(ventas, total_ventas);
-        printf("(Aquí se mostrarían las top 3 más vendidas)\n");
-    } 
-    else if (strcmp(argv[1], "cat") == 0) {
-        // ventas_por_categoria(ventas, total_ventas);
-        printf("(Aquí se mostraría la agrupación por categoría)\n");
-    } 
-    else {
-        printf("Comando desconocido: %s\n", argv[1]);
+    for (int i = 2; i < argc; i++) {
+        if (strcmp(argv[i], "pms") == 0) {
+            pizza_mas_vendida(ventas, total_ventas);
+        } else if (strcmp(argv[i], "pls") == 0) {
+            pizza_menos_vendida(ventas, total_ventas);
+        } else if (strcmp(argv[i], "cat") == 0) {
+            printf("(Aquí se mostraría la agrupación por categoría)\n");
+        } else {
+            printf("Comando desconocido: %s\n", argv[i]);
+        }
     }
 
     return 0;
