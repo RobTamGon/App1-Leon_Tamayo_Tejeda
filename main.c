6db280f62faf14bce00c6d25468112dc1cbdd58c
Alonso Tamayo <Alotamgon@gmail.com>
2025-04-04 20:24:44 -0300

    Progreso V2 en main.c
    Ahora están implementados correctamente los punteros de funciones (por medio de una lista de punteros a las funciones de métricas).
    
    Ahora sigue fusionar las primeras funciones de métricas para probar el flujo del programa completo.

--- main.c
+++ main.c
@@ -1,93 +1,115 @@
 #include <stdio.h>
+#include <string.h>
 
 //ESTA PARTE ES TEMPORAL, SOLAMENTE PARA PROBAR
-struct Venta
+struct order
 {
 	int a;
-	int b;
 };
 
-char* f1(int *size, struct Venta *Ventas)
+char* f1(int *size, struct order *orders)
 {
-	return "f1";
+	return "f1 ";
 }
 
-char* f2(int *size, struct Venta *Ventas)
+char* f2(int *size, struct order *orders)
 {
-	return "f2";
+	return "f2 ";
 }
 
-char* f3(int *size, struct Venta *Ventas)
+char* f3(int *size, struct order *orders)
 {
-	return "f3";
+	return "f3 ";
 }
 
-char* f4(int *size, struct Venta *Ventas)
+char* f4(int *size, struct order *orders)
 {
-	return "f4";
+	return "f4 ";
 }
 
-char* f5(int *size, struct Venta *Ventas)
+char* f5(int *size, struct order *orders)
 {
-	return "f5";
+	return "f5 ";
 }
 
-char* f6(int *size, struct Venta *Ventas)
+char* f6(int *size, struct order *orders)
 {
-	return "f6";
+	return "f6 ";
 }
 
-char* f7(int *size, struct Venta *Ventas)
+char* f7(int *size, struct order *orders)
 {
-	return "f7";
+	return "f7 ";
 }
 
-char* f8(int *size, struct Venta *Ventas)
+char* f8(int *size, struct order *orders)
 {
-	return "f8";
+	return "f8 ";
 }
 
-char* f9(int *size, struct Venta *Ventas)
+char* f9(int *size, struct order *orders)
 {
-	return "f9";
+	return "f9 ";
 }
 
-char* f10(int *size, struct Venta *Ventas)
+char* f10(int *size, struct order *orders)
 {
-	return "f10";
+	return "f10 ";
 }
+
+
+#define LARGO_PARAMETROS 10
 
 
 int main(int argc, char *argv[])
 {
-	printf("Argument Count: %i\n", argc);
-	for (int i = 0; i < argc; i++)
+	// Definición del tipo de variable que es el puntero a una de las funciones de las métricas
+	typedef char* (*PunteroFuncion)(int*, struct order*);
+
+
+	// Imprime ayuda si el programa no recibe suficientes argumentos
+	if (argc <= 2) {
+        printf("No se ingresaron suficientes argumentos, intente ingresar alguna(s) de las siguientes opciones disponibles:\n");
+        printf("- pms:     Pizza mas vendida\n");
+        printf("- pls:     Pizza menos vendida\n");
+        printf("- dms:     Fecha con mas ventas en terminos de dinero (junto a la cantidad de dinero recaudado)\n");
+        printf("- dls:     Fecha con menos ventas en terminos de dinero (junto a la cantidad de dinero recaudado)\n");
+        printf("- dmsp:    Fecha con mas ventas en terminos de cantidad de pizzas (junto a la cantidad de pizzas)\n");
+        printf("- dlsp:    Fecha con menos ventas en terminos de cantidad de pizzas (junto a la cantidad de pizzas)\n");
+        printf("- apo:     Promedio de pizzas por orden\n");
+        printf("- apd:     Promedio de pizzas por dia\n");
+        printf("- ims:     Ingrediente mas vendido\n");
+        printf("- hp:      Cantidad de pizzas por categoria vendidas\n\n");
+		printf("Por ejemplo:\n./app1 ventas.csv pms pls dms dls dmsp dlsp apo apd ims hp\n\n");
+        return 0;
+    }
+
+
+	// SECCIÓN PARA LEER Y PARAMETRIZAR LAS ÓRDENES
+	int largo_ordenes = 20;
+	struct order ordenes[largo_ordenes];
+
+
+	// Parámetros que se reconocerán en la línea de comando
+	char* parametros[LARGO_PARAMETROS] = {"pms", "pls", "dms", "dls", "dmsp", "dlsp", "apo", "apd", "ims", "hp"};
+
+	// Lista de punteros a las funciones de las métricas, asociada implícitamente por el índice con la lista de parámetros anterior
+	// (Ejemplo: "pms" en el índice 0 se asocia a f1, también en el índice 0)
+	const PunteroFuncion punteros[LARGO_PARAMETROS] = {f1, f2, f3, f4, f5, f6, f7, f8, f9, f10};
+	
+
+	// Se itera por cada parámetro ingresado después del nombre del programa y del nombre del archivo a leer para comparar con los parámetros que se reconocen
+	for (int arg_i = 2; arg_i < argc; arg_i++)
 	{
-		printf("%s\n", argv[i]);
+		// Se itera por cada parámetro aceptado y se compara con el parámetro ingresado, si coinciden, se ejecuta la función asociada
+		for (int i = 0; i < LARGO_PARAMETROS; i++)
+		{
+			if (strcmp(argv[arg_i], parametros[i]) == 0)
+			{
+				printf("%s", punteros[i](&largo_ordenes, ordenes));
+			}
+		}
 	}
-
-	struct Venta V;
-	V.a = 1;
-	V.b = 2;
-
-	int T = 10;
-
-	char* (*pointer)(int*, struct Venta*) = f1;
-
-	printf("%s\n\n", (*pointer)(&T, &V));
-
-	char* (*metricas[argc - 2])(int*, struct Venta*);
-
-	for (int i = 2; i < argc; i++)
-	{
-		//O UN SWITCH PARA CADA MÉTRICA
-		// O UNA LISTA DE PARÁMETROS, Y UNA LISTA DE PUNTEROS
-		//QUIZÁS EN UNA FUNCIÓN DEDICADA A CONVERTIR LOS PARÁMETROS EN UNA LISTA DE PUNTEROS A FUNCIONES
-		printf("%s\n", argv[i]);
-	}
-
-
-
 
 
 	return 0;
