113936b2efc16274008f34299270988edd517908
Alonso Tamayo <Alotamgon@gmail.com>
2025-04-04 00:31:44 -0300

    Progreso V1 en main.c
    Avancé y tengo casi listo (con funciones y una estructura de prueba) la forma que tendrá el main.c para utilizar punteros a funciones.
    
    Falta decidir si usar un switch para asociar los parámetros a sus funciones (aunque eso como que quita la utilidad de punteros a funciones), o usar una función aparte que haga la conversión a punteros de funciones (sea con el mismo switch o asociando una lista de posibles parámetros con una de punteros a las funciones, compartiendo índice), y luego se itere llamando a cada función en la lista de punteros (que creo que será mejor para separar responsabilidades y hacer más mantenible y fácilmente escalable el código.
    
    ¿Qué pasaría si una función tuviera varios parámetros que la llaman? ¿Las listas de asociaciones repiten puntero y ya?

--- main.c
+++ main.c
@@ -1,6 +1,94 @@
 #include <stdio.h>
 
+//ESTA PARTE ES TEMPORAL, SOLAMENTE PARA PROBAR
+struct Venta
+{
+	int a;
+	int b;
+};
+
+char* f1(int *size, struct Venta *Ventas)
+{
+	return "f1";
+}
+
+char* f2(int *size, struct Venta *Ventas)
+{
+	return "f2";
+}
+
+char* f3(int *size, struct Venta *Ventas)
+{
+	return "f3";
+}
+
+char* f4(int *size, struct Venta *Ventas)
+{
+	return "f4";
+}
+
+char* f5(int *size, struct Venta *Ventas)
+{
+	return "f5";
+}
+
+char* f6(int *size, struct Venta *Ventas)
+{
+	return "f6";
+}
+
+char* f7(int *size, struct Venta *Ventas)
+{
+	return "f7";
+}
+
+char* f8(int *size, struct Venta *Ventas)
+{
+	return "f8";
+}
+
+char* f9(int *size, struct Venta *Ventas)
+{
+	return "f9";
+}
+
+char* f10(int *size, struct Venta *Ventas)
+{
+	return "f10";
+}
+
+
 int main(int argc, char *argv[])
 {
+	printf("Argument Count: %i\n", argc);
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	struct Venta V;
+	V.a = 1;
+	V.b = 2;
+
+	int T = 10;
+
+	char* (*pointer)(int*, struct Venta*) = f1;
+
+	printf("%s\n\n", (*pointer)(&T, &V));
+
+	char* (*metricas[argc - 2])(int*, struct Venta*);
+
+	for (int i = 2; i < argc; i++)
+	{
+		//O UN SWITCH PARA CADA MÉTRICA
+		// O UNA LISTA DE PARÁMETROS, Y UNA LISTA DE PUNTEROS
+		//QUIZÁS EN UNA FUNCIÓN DEDICADA A CONVERTIR LOS PARÁMETROS EN UNA LISTA DE PUNTEROS A FUNCIONES
+		printf("%s\n", argv[i]);
+	}
+
+
+
+
+
 	return 0;
 }
