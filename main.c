ed7f98890185d85f54580b47816b776cb159399b
ignacioleonriquelme <ignacioleonriquelme@gmail.com>
2025-04-04 21:56:49 -0300

    Prueba de las funciones en main.c

--- main.c
+++ main.c
@@ -17,21 +17,37 @@ int main(int argc, char* argv[]) {
 
     printf("Se cargaron %d órdenes:\n", contador_ventas);
     for (int i = 0; i < contador_ventas; i++) {
-        printf("%d - %s - %d unidades\n", ventas[i].order_id, ventas[i].pizza_name, ventas[i].quantity);
+        printf("%d - %s - %.2f unidades\n", ventas[i].order_id, ventas[i].pizza_name, ventas[i].quantity);
     }
 
     // Imprime la fecha con más ventas en cantidad de pizzas
     printf("%s" , encontrar_fecha_mas_vendida(&contador_ventas, ventas));
+    printf("\n");
 
     // Imprime la fecha con más ventas en dinero
     printf("%s", encontrar_fecha_mas_ingresos(&contador_ventas, ventas));
+    printf("\n");
 
     // Imprime la fecha con menos ventas en cantidad de pizzas
     printf("%s", encontrar_fecha_menos_vendida(&contador_ventas, ventas));
+    printf("\n");
 
     // Imprime la fecha con menos ventas en dinero
     printf("%s", encontrar_fecha_menos_ingresos(&contador_ventas, ventas));
+    printf("\n");
 
-    //liberar_ventas(ventas);
+    // Imprime la fecha con menos ventas en dinero
+    printf("%s", encontrar_pizza_mas_vendida(&contador_ventas, ventas));
+    printf("\n");
+
+    // Imprime la fecha con menos ventas en dinero
+    printf("%s", encontrar_pizza_menos_vendida(&contador_ventas, ventas));
+    printf("\n");
+
+    // Imprime la fecha con menos ventas en dinero
+    printf("%s", encontrar_ingrediente_mas_vendido(&contador_ventas, ventas));
+    printf("\n");
+
+    liberar_ventas(ventas);
     return 0;
 }
