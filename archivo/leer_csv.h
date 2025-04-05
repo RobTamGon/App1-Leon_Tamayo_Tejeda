c3b60d6e27c878c9e7623832f58641bf79a83bab
Alonso Tamayo <Alotamgon@gmail.com>
2025-04-05 01:49:48 -0300

    Progreso V3 en main.c
    Cambié la idea de asociar elementos de dos listas (una con el nombre de los parámetros y otra con los punteros a las funciones que le corresponden) por sus índices; ahora usé una estructura de datos nueva llamada Metrica para guardar todas las entradas de texto que se puedan asociar a una misma función de métrica, guardando el puntero correspondiente. Luego itero por las 3 capas para encontrar el puntero correcto a usar.
    
    Sugiero añadir después una variable bool en los ciclos para saber si se encontró al menos un parámetro, para que si no, se imprima un mensaje de ayuda informándolo.
    (Alternativamente se podría añadir un aviso para cada parámetro que "falle")

--- archivo/leer_csv.h
+++ archivo/leer_csv.h
@@ -1,7 +1,7 @@
 // Creamos un archivo .h (header) para declarar las funciones, estructuras y constantes que queremos usar desde otros archivos .c, sin tener que copiar el código. 
 // Esto permite organizar el programa en módulos separados, facilita la reutilización del código y mejora el mantenimiento, ya que cualquier cambio se centraliza en un solo lugar.
-#ifndef LECTURA_CSV_H
-#define LECTURA_CSV_H
+#ifndef LEER_CSV_H
+#define LEER_CSV_H
 
 #include "../structs.h"
 
--- main.c
+++ main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h>								// Para imprimir (printf)
+#include <string.h>								// Para comparar Strings fácilmente (strcmp)
 
-#include "structs.h"
-#include "archivo/leer_csv.h"
-#include "metricas/atributo_mayor_menor.h"
+#include "structs.h"							// Contiene la estructura de datos order
+#include "archivo/leer_csv.h"					// Contiene la funcionalidad de leer el archivo de datos y completar las estructuras de datos de tipo order
+#include "metricas/atributo_mayor_menor.h"		// Contiene las funciones para las métricas de categoría Atributo Mayor o Menor: pms (Pizza más vendida), pls (Pizza menos vendida) e ims (Ingrediente más vendido)
+
 
 //ESTA PARTE ES TEMPORAL, SOLAMENTE PARA PROBAR
-
 char* f3(int *size, order *orders)
 {
 	return "f3";
@@ -43,17 +43,44 @@ char* f10(int *size, order *orders)
 }
 
 
-#define MAX_ORDENES 1000
-#define LARGO_PARAMETROS 10
+#define INDICE_PARAMETRO_ARCHIVO 1						// Índice del parámetro que indica el nombre del archivo de datos a leer
+#define PARAMETROS_ANTES_DE_METRICAS 2			// Cantidad de parámetros que deben haber antes de ingresar parámetros asociados a métricas
+#define LARGO_METRICAS 10						// Largo de la lista metricas, es decir, la cantidad de métricas/funciones que el programa reconoce
+#define MAX_ORDENES 1000						// Cantidad máxima de órdenes a guardar [SE PODRÍA ELIMINAR CON UNA FUNCIÓN PREVIA QUE REGRESA LA CANTIDAD DE ÓRDENES SIN RELLENAR LAS ESTRUCTURAS]
+
 
 
 int main(int argc, char *argv[])
 {
-	// Definición del tipo de variable que es el puntero a una de las funciones de las métricas
+	// Define el tipo de variable que es el puntero a una de las funciones de las métricas
 	typedef char* (*PunteroFuncion)(int*, order*);
 
+	// Define el tipo de variable que contiene el puntero a la función de su métrica correspondiente, junto con los Strings que se asocian a esa métrica
+	typedef struct
+	{
+		const int largo_comandos;
+		const char *comandos[2];
+		const PunteroFuncion funcion_metrica;
+	} Metrica;
 
-	// Imprime ayuda si el programa no recibe suficientes argumentos
+
+	// Lista de estructuras de tipo Metrica, es decir, inicializa todas las opciones que reconocerá el programa
+	const Metrica metricas[LARGO_METRICAS] =
+	{
+		{2, {"pms", "pizza_mas_vendida"}, pizza_mas_vendida},
+		{2, {"pls", "pizza_menos_vendida"}, pizza_menos_vendida},
+		{2, {"dms", "encontrar_fecha_mas_vendida"}, f3},
+		{2, {"dls", "encontrar_fecha_menos_vendida"}, f4},
+		{2, {"dmsp", "encontrar_fecha_mas_ingresos"}, f5},
+		{2, {"dlsp", "encontrar_fecha_menos_ingresos"}, f6},
+		{2, {"apo", "promedio_pizzas_por_orden"}, f7},
+		{2, {"apd", "promedio_pizzas_por_dia"}, f8},
+		{2, {"ims", "ingrediente_mas_vendido"}, ingrediente_mas_vendido},
+		{2, {"hp", "cantidad_de_pizzas_por_categoria_vendidas"}, f10}
+	};
+
+
+	// Imprime ayuda si el programa no recibe suficientes parámetros
 	if (argc <= 2) {
         printf("No se ingresaron suficientes argumentos, intente ingresar alguna(s) de las siguientes opciones disponibles:\n");
         printf("- pms:     Pizza mas vendida\n");
@@ -71,28 +98,26 @@ int main(int argc, char *argv[])
     }
 
 
-	// Se lee el archivo ingresado como segundo parámetro en la línea de comando y se guardan las estructuras de las órdenes creadas a partir de esa lectura
+	// Lee el archivo ingresado como segundo parámetro en la línea de comando y guarda las estructuras de las órdenes creadas a partir de esa lectura
 	order ordenes[MAX_ORDENES];
-	int largo_ordenes = leer_csv(argv[1], ordenes, MAX_ORDENES);
+	int largo_ordenes = leer_csv(argv[INDICE_PARAMETRO_ARCHIVO], ordenes, MAX_ORDENES);
 
 
-	// Parámetros que se reconocerán en la línea de comando
-	const char* parametros[LARGO_PARAMETROS] = {"pms", "pls", "dms", "dls", "dmsp", "dlsp", "apo", "apd", "ims", "hp"};
-
-	// Lista de punteros a las funciones de las métricas, asociada implícitamente por el índice con la lista de parámetros anterior
-	// (Ejemplo: "pms" en el índice 0 se asocia a pizza_mas_vendida, que también está en el índice 0)
-	const PunteroFuncion punteros[LARGO_PARAMETROS] = {pizza_mas_vendida, pizza_menos_vendida, f3, f4, f5, f6, f7, f8, ingrediente_mas_vendido, f10};
-
-
-	// Se itera por cada parámetro ingresado después del nombre del programa y del nombre del archivo a leer para comparar con los parámetros que se reconocen
-	for (int arg_i = 2; arg_i < argc; arg_i++)
+	// De manera general: itera por cada parámetro, métrica, y comando reconocido para encontrar la métrica correcta asociada al parámetro ingresado
+	// Específicamente: itera por cada parámetro desde el primero que debe considerarse asociado a una métrica
+	for (int arg_i = PARAMETROS_ANTES_DE_METRICAS; arg_i < argc; arg_i++)
 	{
-		// Se itera por cada parámetro que se reconocerá y se compara con el parámetro ingresado, si coinciden, se ejecuta la función asociada
-		for (int i = 0; i < LARGO_PARAMETROS; i++)
+		// Itera por cada estructura de tipo Metrica
+		for (int metrica_i = 0; metrica_i < LARGO_METRICAS; metrica_i++)
 		{
-			if (strcmp(argv[arg_i], parametros[i]) == 0)
+			// Itera por cada comando reconocido en la estructura Metrica y los compara con el parámetro ingresado
+			// Si coinciden, ejecuta la función asociada en el puntero dentro de la estructura Metrica en la que se encuentre la coincidencia, e imprime el resultado
+			for (int comando_i = 0; comando_i < metricas[metrica_i].largo_comandos; comando_i++)
 			{
-				printf("- %s\n", punteros[i](&largo_ordenes, ordenes));
+				if (strcmp(argv[arg_i], metricas[metrica_i].comandos[comando_i]) == 0)
+				{
+					printf("- %s\n", metricas[metrica_i].funcion_metrica(&largo_ordenes, ordenes));
+				}
 			}
 		}
 	}
