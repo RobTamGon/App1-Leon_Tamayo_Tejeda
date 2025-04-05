df63f08fba4550fa690ecf70d9a54d32755f496d
ignacioleonriquelme <ignacioleonriquelme@gmail.com>
2025-04-04 21:51:15 -0300

    Modificación de la estructura de ventas, de acuerdo con la estructura de leer_csv.h en leer_csv.hc

--- leer_csv.c
+++ leer_csv.c
@@ -11,13 +11,19 @@ Venta* leer_csv(const char* nombre_archivo, int* largo) {
     }
 
     char linea[MAX_LINE_LENGTH];
-    *largo = 0;
+    *largo = 0; // Inicializamos el contador de líneas a 1 para la segunda línea (cabecera)
 
+    // Leer la primera línea del archivo
+    fgets(linea, sizeof(linea), archivo); // Ignoramos la cabecera
+    
     // Contar líneas para saber cuántas órdenes hay
     while (fgets(linea, sizeof(linea), archivo)) {
         (*largo)++;
     }
     rewind(archivo); // Volvemos al inicio del archivo
+
+    // Leer la primera línea del archivo
+    fgets(linea, sizeof(linea), archivo); // Ignoramos la cabecera
 
     // Reservamos memoria para todas las órdenes
     Venta* ventas = malloc((*largo) * sizeof(Venta));
@@ -30,7 +36,7 @@ Venta* leer_csv(const char* nombre_archivo, int* largo) {
     // Leer las líneas y parsear datos
     int i = 0;
     while (fgets(linea, sizeof(linea), archivo)) {
-        sscanf(linea, "%d,%d,%49[^,],%lf,%14[^,],%9[^,],%lf,%lf,%c,%19[^,],\"%199[^\"]\",%49[^\n]",
+        sscanf(linea, "%d,%d,%49[^,],%f,%14[^,],%9[^,],%f,%f,%c,%19[^,],\"%199[^\"]\",%49[^\n]",
             &ventas[i].pizza_id, &ventas[i].order_id, ventas[i].pizza_name_id,
             &ventas[i].quantity, ventas[i].order_date, ventas[i].order_time,
             &ventas[i].unit_price, &ventas[i].total_price, &ventas[i].pizza_size,
